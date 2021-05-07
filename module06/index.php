8faf0cd1688f201a3ceffacdc93e555f2d7a9f12
Juliana Pereira <jotace1234@hotmail.com>
2021-05-07 11:32:02 -0300

    update

--- module06/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $array1 = array("cor" => "vermelho",2,4);
-    $array2 = array("a","b","cor" => "verde","forma" => "trapezóide",4);
-    $result = array_merge($array1,$array2);
-    print_r($result);
-?>
